1c2a41eb628c3b2d0677cc93e90904b80bf3af5a
siswonugroho <User@LAPTOP-GFO118AO>
2020-06-13 12:05:20 +0700

    Modify connect class

--- src/com/sovana/sewa/connection/Connect.java
+++ src/com/sovana/sewa/connection/Connect.java
@@ -13,37 +13,38 @@ public class Connect {
 
     private static Connection mysqlconfig;
 
-    public static void startMysql() {
-        JOptionPane opt = new JOptionPane();
-        opt.setMessage("Memulai MySQL server...");
-        opt.setMessageType(1);
-        opt.setVisible(true);
-        
-                
-        String command = "C:\\xampp\\mysql\\bin\\mysqld.exe";
-        EventQueue.invokeLater(() -> {
-            try {
-                Process process = Runtime.getRuntime().exec(command);
-                if (process.isAlive()) {
-                    JOptionPane.showMessageDialog(null, "MySQL Server sudah berjalan.", "Info", 1);
-                }
-            } catch (HeadlessException | IOException e) {
-                JOptionPane.showMessageDialog(null, "Terjadi error:\n" + e.getMessage(), "Error", 0);
-            }
-        });
-        opt.setVisible(false);
-
-    }
+//    public static void startMysql() {
+//        JOptionPane opt = new JOptionPane();
+//        opt.setMessage("Memulai MySQL server...");
+//        opt.setMessageType(1);
+//        opt.setVisible(true);
+//        
+//                
+//        String command = "C:\\xampp\\mysql\\bin\\mysqld.exe";
+//        EventQueue.invokeLater(() -> {
+//            try {
+//                Process process = Runtime.getRuntime().exec(command);
+//                if (process.isAlive()) {
+//                    JOptionPane.showMessageDialog(null, "MySQL Server sudah berjalan.", "Info", 1);
+//                }
+//            } catch (HeadlessException | IOException e) {
+//                JOptionPane.showMessageDialog(null, "Terjadi error:\n" + e.getMessage(), "Error", 0);
+//            }
+//        });
+//        opt.setVisible(false);
+//
+//    }
 
     public static Connection configDB() throws SQLException {
         try {
+            Class.forName("com.mysql.jdbc.Driver");
             String url = "jdbc:mysql://localhost/sewaterop"; //url database
             String user = "root"; //user database    public static Connection configDB() throws SQLException{
 
             String pass = ""; //password database
-            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+//            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
             mysqlconfig = DriverManager.getConnection(url, user, pass);
-        } catch (SQLException e) {
+        } catch (SQLException | ClassNotFoundException e) {
             JOptionPane.showMessageDialog(null, "Gagal terhubung ke database.\nPenyebab:\n" + e.getMessage(), "Error", 0); //perintah menampilkan error pada koneksi
             System.exit(0);
         }
